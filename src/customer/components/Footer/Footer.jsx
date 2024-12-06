e157c78149aa5a52572f9f134367aac6d175bbad
Sai Navadeep <2200030032@kluniversity.in>
2024-12-06 15:19:49 +0530

    Frontend

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -1,84 +1,93 @@
-import React from 'react';
+import { Grid, Link, Typography } from '@mui/material';
 
-import { Button, Grid, Grid2, Typography } from '@mui/material'
 const Footer = () => {
-    return (
-        <div>
-            <Grid className= 'bg-black text-white text-center mt-10' 
-            container 
-            sx={{bgcolor:"black",color:"white",py:3}}  
-            >
-                <Grid item xs={12} sm={6} md={3}>
-                <Typography className='pb-5' variant='h6'>Company</Typography>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>About</Button>
-                </div>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>Blog</Button>
-                </div><Button className='pb-5' variant='h6'gutterBottom>Press</Button>
-                <div><Button className='pb-5' variant='h6'gutterBottom>Jobs</Button>
-                </div><Button className='pb-5' variant='h6'gutterBottom>Partners</Button>
-                </Grid>
+  return (
+    <Grid className='bg-black text-white mt-10 text-center' container color={'white' } sx={{ bgcolor: 'black', color: 'white', py: 3 }}>
+      <Grid  item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Company
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          About
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Blog
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Jobs
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Press
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Partners
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Solutions
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Marketing
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Analytics
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Commerce
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Insights
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Support
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Documentation
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Guides
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          API Status
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Typography className='pb-5' variant="h6" gutterBottom>
+          Legal
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Claim
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Privacy
+        </Typography>
+        <Typography variant="body2" component="p" gutterBottom>
+          Terms
+        </Typography>
+      </Grid>
+      <Grid className='pt-20' item xs={12} >
+        <Typography variant="body2" component="p" align="center">
+          &copy; 2023 My Company. All rights reserved.
+        </Typography>
+        <Typography variant="body2" component="p" align="center">
+          Made with love by Me.
+        </Typography>
+        <Typography variant="body2" component="p" align="center">
+          Icons made by{' '}
+          <Link href="https://www.freepik.com" color="inherit" underline="always">
+            Freepik
+          </Link>{' '}
+          from{' '}
+          <Link href="https://www.flaticon.com/" color="inherit" underline="always">
+            www.flaticon.com
+          </Link>
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
-
-
-                <Grid item xs={12} sm={6} md={3}>
-                <Typography className='pb-5' variant='h6'>Solutions</Typography>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>Marketing</Button>
-                </div>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>Analytics</Button>
-                </div><Button className='pb-5' variant='h6'gutterBottom>Commerce</Button>
-                <div><Button className='pb-5' variant='h6'gutterBottom>Insights</Button>
-                </div><Button className='pb-5' variant='h6'gutterBottom>Support</Button>
-                </Grid>
-
-
-
-
-                <Grid item xs={12} sm={6} md={3}>
-                <Typography className='pb-5' variant='h6'>Documentation</Typography>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>Guides</Button>
-                </div>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>API Status</Button>
-                </div>
-                
-                </Grid>
-
-
-
-                <Grid item xs={12} sm={6} md={3}>
-                <Typography className='pb-5' variant='h6'>Legal</Typography>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>claim</Button>
-                </div>
-                <div>
-                <Button className='pb-5' variant='h6'gutterBottom>Privacy</Button>
-                </div><Button className='pb-5' variant='h6'gutterBottom>Terms</Button>
-            
-                </Grid>
-
-
-
-                </Grid>
-<Grid className=' pt-20' item xs={12}>
-<Typography variant="body2" component="p" align="center">
-&copy; 2023 My Company. All rights reserved.
-</Typography>
-< Typography variant="body2" component="p" align="center">
-Made with love by Me.
-</Typography>
-<Typography variant="body2" component="p" align="center">
-</Typography>
-</Grid>
-
-
-            
-      </div>  
-    )
-}
-
-export default Footer
+export default Footer;
